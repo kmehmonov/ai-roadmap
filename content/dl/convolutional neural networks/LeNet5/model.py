94c3d3932fc498f375aab25668ff7de3e72e8bde
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2025-05-04 15:08:10 +0500

    some changes

--- content/dl/convolutional neural networks/LeNet5/model.py
+++ content/dl/convolutional neural networks/LeNet5/model.py
@@ -2,11 +2,10 @@ import torch
 import torch.nn as nn
 
 
-
 class LeNet5(nn.Module):
     def __init__(self):
         super().__init__()
-        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
         self.pool = nn.AvgPool2d(kernel_size=(2,2), stride=(2,2))
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(5,5), stride=(1,1), padding=(0,0))
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=(5,5), stride=(1,1), padding=(0,0))
@@ -15,13 +14,13 @@ class LeNet5(nn.Module):
         self.linear2 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
+        x = self.tanh(self.conv1(x))
         x = self.pool(x)
-        x = self.relu(self.conv2(x))
+        x = self.tanh(self.conv2(x))
         x = self.pool(x)
-        x = self.relu(self.conv3(x))
+        x = self.tanh(self.conv3(x))
         x = x.reshape(x.shape[0], -1)
-        x = self.relu(self.linear1(x))
+        x = self.tanh(self.linear1(x))
         x = self.linear2(x)
         return x
 
